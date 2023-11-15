529f39fb208b60fcda85bd8d49ea628f6cbcc875
hyesulim <hyesulim@kaist.ac.kr>
2023-11-15 02:43:44 +0000

    [Add] config and function for loading saved model

--- linear_probe/main.py
+++ linear_probe/main.py
@@ -1,12 +1,16 @@
 # %%
 import sys
+
 sys.path.append("../")
+
+import os
 
 import torch
 import torch.nn as nn
 
 import clip
 from linear_probe.dataset import get_combined_loader
+from linear_probe.helpers import *
 from linear_probe.trainer import train
 
 # %%
@@ -18,27 +22,28 @@ model, preprocess = clip.load(backbone, device=device)
 model = model.visual
 
 model.to(device)
-# %%
-ROOT = "/home/nas2_userH/hyesulim/Data"
 
 config = {
+    "root_data": "/home/nas2_userH/hyesulim/Data",
+    "root_code": "/home/nas2_userH/hyesulim/Dev/2023/11785-f23-prj/linear_probe",
     "device": device,
     "optim": "adam",
     "lr": 1e-3,
     "num_epochs": 10,
     "obj": "layer1_2_relu3",
     "batch_size": 128,
-    "subset_samples": 10000
+    "subset_samples": 10000,
+    "ckpt_dir": "/home/nas2_userH/hyesulim/Dev/2023/11785-f23-prj/linear_probe/logs/layer1_2_relu3/version_1"
+    # "ckpt_dir": None
 }
-
 
 # Load data
 
 data_loader = get_combined_loader(
-    root_dir=ROOT,
+    root_dir=config["root_data"],
     transform=preprocess,
-    batch_size=config["batch_size"], 
-    subset_samples=config["subset_samples"]
+    batch_size=config["batch_size"],
+    subset_samples=config["subset_samples"],
 )
 
 # Linear probing model
@@ -48,12 +53,13 @@ out_dim = 2  # discriminate two datasets
 linear_probe = nn.Linear(in_dim, out_dim)
 linear_probe.to(device)
 
-linear_probe = train(
-    model, 
-    linear_probe, 
-    data_loader, 
-    config, 
-    obj=config["obj"]
-)
+if config["train"]:
+    linear_probe, load_dir = train(
+        model, linear_probe, data_loader, config, obj=config["obj"]
+    )
 
+elif config["ckpt_dir"] is not None:
+    load_dir = config["ckpt_dir"]
+
+loaded_model = load_model(linear_probe, load_dir)
 # %%
