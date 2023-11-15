1f6e2f7244bacd863154cf7ea576b982fffe1ec9
hyesulim <hyesulim@kaist.ac.kr>
2023-11-15 02:43:02 +0000

    [Add] log message, save and load models

--- linear_probe/helpers.py
+++ linear_probe/helpers.py
@@ -1,4 +1,7 @@
+import os
+
 import matplotlib.pyplot as plt
+import torch
 
 
 def plot_images(dataloader):
@@ -17,3 +20,48 @@ def plot_images(dataloader):
         ax[i].axis("off")  # Turn off axis numbers and labels
 
     plt.show()
+
+
+def make_save_dir(base_dir):
+    # Ensure the base directory exists
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+
+    # Find the next version number
+    existing_versions = [
+        d
+        for d in os.listdir(base_dir)
+        if os.path.isdir(os.path.join(base_dir, d)) and d.startswith("version_")
+    ]
+    if existing_versions:
+        latest_version = max([int(v.split("_")[1]) for v in existing_versions])
+        next_version = latest_version + 1
+    else:
+        next_version = 1
+
+    # Create new version directory
+    version_dir = os.path.join(base_dir, f"version_{next_version}")
+    os.makedirs(version_dir)
+
+    return version_dir
+
+
+def log_message(message, save_dir):
+    file_path = os.path.join(save_dir, "training_log.txt")
+    with open(file_path, "a") as file:
+        file.write(message + "\n")
+
+
+def save_model(model, save_dir):
+    # Save the model checkpoint
+    checkpoint_path = os.path.join(save_dir, "model_checkpoint.pth")
+    torch.save(model.state_dict(), checkpoint_path)
+    print(f"Model saved to {checkpoint_path}")
+
+    return checkpoint_path
+
+
+def load_model(model, load_dir):
+    checkpoint_path = os.path.join(load_dir, "model_checkpoint.pth")
+    model.load_state_dict(torch.load(checkpoint_path))
+    return model
