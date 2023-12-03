648045777e41e8813f961cb4f8eab6f7a0c12792
hyesulim <hyesulim@kaist.ac.kr>
2023-12-03 22:19:44 +0000

    [Reformat]

--- faceted_visualization/visualizer/main.py
+++ faceted_visualization/visualizer/main.py
@@ -28,7 +28,9 @@ def get_probe_weights(model_location: str) -> torch.Tensor:
         return linear_probe["weight"][0]
 
 
-def get_model(model_name) -> Tuple[torch.nn.Module, torchvision.transforms.transforms.Compose]:
+def get_model(
+    model_name,
+) -> Tuple[torch.nn.Module, torchvision.transforms.transforms.Compose]:
     logger.info("Loading CLIP model [ %s ].", model_name)
     if model_name in clip.available_models():
         model, transforms = clip.load(model_name, device=device)
@@ -40,7 +42,11 @@ def get_model(model_name) -> Tuple[torch.nn.Module, torchvision.transforms.trans
         logger.info("Finished loading transforms [ %s ]", transforms)
         return model, transforms
     else:
-        raise RuntimeError("Unable to locate CLIP model. Possible values are {!s}".format(clip.available_models()))
+        raise RuntimeError(
+            "Unable to locate CLIP model. Possible values are {!s}".format(
+                clip.available_models()
+            )
+        )
 
 
 def get_optimizer(parameters, optimizer_name: str, learning_rate: float):
@@ -55,45 +61,61 @@ def get_optimizer(parameters, optimizer_name: str, learning_rate: float):
         return torch.optim.SGD(parameters, lr=learning_rate)
 
 
-def orchestrate(config: Dict, wandb_object: wb.WandB = None, save_to_file: bool = False) -> Callable:
+def orchestrate(
+    config: Dict, wandb_object: wb.WandB = None, save_to_file: bool = False
+) -> Callable:
     if save_to_file:
         logger_.add_file_handler(config)
     model, transforms = get_model(config[constants.MODEL])
 
     model_hook = hook.register_hooks(model)
 
-    params, image_f = image.generate_img(w=config[constants.IMAGE_WIDTH],
-                                         h=config[constants.IMAGE_HEIGHT],
-                                         decorrelate=bool(config[constants.IMG_DECORRELATE]),
-                                         fft=bool(config[constants.IMG_FFT]), device=device)
+    params, image_f = image.generate_img(
+        w=config[constants.IMAGE_WIDTH],
+        h=config[constants.IMAGE_HEIGHT],
+        decorrelate=bool(config[constants.IMG_DECORRELATE]),
+        fft=bool(config[constants.IMG_FFT]),
+        device=device,
+    )
 
-    optimizer = get_optimizer(parameters=params, optimizer_name=config[constants.OPTIMIZER],
-                              learning_rate=config[constants.LEARNING_RATE])
+    optimizer = get_optimizer(
+        parameters=params,
+        optimizer_name=config[constants.OPTIMIZER],
+        learning_rate=config[constants.LEARNING_RATE],
+    )
 
-    probe_weights = get_probe_weights(model_location=config[constants.PATH_LINEAR_PROBE])
+    probe_weights = get_probe_weights(
+        model_location=config[constants.PATH_LINEAR_PROBE]
+    )
 
     if not config[constants.USE_TRANSFORMS]:
         transforms = None
 
-    render.optimize(num_iterations=config[constants.NUMBER_OF_ITERATIONS],
-                    transforms=transforms,
-                    image_function=image_f,
-                    model=model,
-                    channel=config[constants.CHANNEL],
-                    objective=config[constants.OBJECTIVE],
-                    layer=config[constants.VISUALIZATION_LAYER],
-                    linear_probe_layer=config[constants.LINEAR_PROBE_LAYER],
-                    probe_weights=probe_weights,
-                    optimizer=optimizer,
-                    model_hook=model_hook,
-                    neuron_x=config.get(constants.NEURON_X, None),
-                    neuron_y=config.get(constants.NEURON_Y, None),
-                    wandb_object=wandb_object,
-                    run_id=run_id)
+    render.optimize(
+        num_iterations=config[constants.NUMBER_OF_ITERATIONS],
+        transforms=transforms,
+        image_function=image_f,
+        model=model,
+        channel=config[constants.CHANNEL],
+        objective=config[constants.OBJECTIVE],
+        layer=config[constants.VISUALIZATION_LAYER],
+        linear_probe_layer=config[constants.LINEAR_PROBE_LAYER],
+        probe_weights=probe_weights,
+        optimizer=optimizer,
+        model_hook=model_hook,
+        neuron_x=config.get(constants.NEURON_X, None),
+        neuron_y=config.get(constants.NEURON_Y, None),
+        wandb_object=wandb_object,
+        run_id=run_id,
+    )
 
-    helpers.save_results(image_array=image_f(),
-                         output_directory=os.path.join(config[constants.PATH_OUTPUT], run_id),
-                         create_dir=True)
+    helpers.save_results(
+        image_array=image_f(),
+        output_directory=os.path.join(
+            config[constants.PATH_OUTPUT], config[constants.LINEAR_PROBE_LAYER], run_id
+        ),
+        create_dir=True,
+    )
 
     return image_f
 
@@ -116,11 +138,10 @@ if __name__ == "__main__":
             entity=properties[constants.WANDB_ENTITY],
             project=properties[constants.WANDB_PROJECT],
             run_name=properties.get(constants.WANDB_RUN_NAME, None),
-            config=properties
+            config=properties,
         )
         orchestrate(config=properties, save_to_file=True, wandb_object=wandb_object_)
     except Exception as e:
         logger.exception("Something went wrong.")
         if wandb_object_ is not None:
             wandb_object_.close()
-
