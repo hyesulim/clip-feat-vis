ddf23970d719fb968ef433269dee1d128c431e63
hyesulim <hyesulim@kaist.ac.kr>
2023-12-03 22:20:08 +0000

    [Add] HS dir, linear probe layers options

--- faceted_visualization/scripts/run.sh
+++ faceted_visualization/scripts/run.sh
@@ -15,19 +15,20 @@ else
 fi
 
 # Paths to be set by the user
-FACETED_VIS_HOME=/Users/rohan/repositories/11785-f23-prj/faceted_visualization/
-CONFIG_FILE_PATH=/Users/rohan/repositories/11785-f23-prj/faceted_visualization/visualizer/config/run_configs.json
-LINEAR_PROBE_PATH=/Users/rohan/repositories/11785-f23-prj/faceted_visualization/linear_probes/model_checkpoint\ \(1\).pth
+FACETED_VIS_HOME=/home/nas2_userH/hyesulim/Dev/2023/11785-f23-prj/faceted_visualization
+CONFIG_FILE_PATH=/home/nas2_userH/hyesulim/Dev/2023/11785-f23-prj/faceted_visualization/visualizer/config/run_configs.json
+LINEAR_PROBE_PATH=/home/nas2_userH/hyesulim/Dev/2023/11785-f23-prj/faceted_visualization/linear_probes/celeba/RN50x4
 
 # model settings
-MODELS=("RN50")
-LAYERS=("layer4_2_conv3")
-LINEAR_PROBE_LAYERS=("layer1_2_relu3")
+MODELS=("RN50x4")
+LAYERS=("layer4_5_conv3")
+# LINEAR_PROBE_LAYERS=("layer1_0_conv3" "layer1_3_conv3" "layer2_5_conv3" "layer3_9_conv3")
+LINEAR_PROBE_LAYERS=("layer1_3_relu3" "layer2_5_relu3")
 OBJECTIVES=("channel" "neuron")
 
 OPTIMIZERS=("AdamW" "Adam" "SGD")
 ITERATIONS=(512)
-LEARNING_RATES=(1e-5)
+LEARNING_RATES=(5e-2)
 CHANNELS=(512)
 NEURON_XS=(3)
 NEURON_YS=(3)
@@ -38,8 +39,8 @@ IMAGE_HS=(224)
 WANDB_RUN_NAME="local-testing-with-transforms"
 FFT=(0 1)
 DECORRELATE=(0 1)
-WANDBFLAGS=("--no-wandb" "--wandb")
-TRANSFORMSFLAGS=("--no-use-transforms" "--use-transforms")
+WANDBFLAGS=("--no-wandb") # "--wandb" 
+TRANSFORMSFLAGS=("--use-transforms") #"--no-use-transforms" 
 
 for model in ${MODELS[@]}; do
   for layer in ${LAYERS[@]}; do
@@ -60,10 +61,10 @@ for model in ${MODELS[@]}; do
                                 # you can change string like this: 
                                 # "$model"_"$layer"_"$lp"_"$obj"_"$opt"_"$it"_"$lr"_"$channel"
                                 WANDB_RUN_NAME="$WANDB_RUN_NAME" 
-                                python $FACETED_VIS_HOME/visualizer/main.py \
+                                CUDA_VISIBILE_DEVICES=1 python $FACETED_VIS_HOME/visualizer/main.py \
                                   --random-seed $RANDOM_SEED \
                                   --config-file-path $CONFIG_FILE_PATH \
-                                  --linear-probe-path "$LINEAR_PROBE_PATH" \
+                                  --linear-probe-path "$LINEAR_PROBE_PATH/$lp.pth" \
                                   --model $model \
                                   --layer $layer \
                                   --linear-probe-layer $lp \
